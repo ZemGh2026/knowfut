65f5f7d90204996c80f1310d9c066b4920be8c10
Your Name <youremail@yourdomain.com>
2026-06-10 08:07:26 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -260,6 +260,7 @@ export default function Home() {
           <span>© 2026 KnowFut</span>
         </div>
       </footer>
+      <span style={{ display: 'none' }}>Impact-Site-Verification: a2a0c509-523b-48fe-9b6d-7485d9eca1c2</span>
     </div>
   );
 }
